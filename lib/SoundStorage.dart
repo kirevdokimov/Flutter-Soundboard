2765438b6b034a3ac01fe5839ebfcec76a75636d
Nobinator <nobinator.root@gmail.com>
2018-04-17 03:19:49 +0300

    Saving filePaths to shared_prefs

--- lib/SoundStorage.dart
+++ lib/SoundStorage.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:io';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SoundStorage{
 
@@ -15,6 +16,12 @@ class SoundStorage{
     'assets/sounds/long.wav'
   ];
 
+  Future<SharedPreferences> prefs;
+
+  SoundStorage(){
+    prefs = SharedPreferences.getInstance();
+  }
+
   Future<List<String>> loadSounds() async{
     List<String> filePaths = new List<String>();
     await Future.forEach(soundAssetPath, (assetPath) async {
@@ -26,14 +33,23 @@ class SoundStorage{
   }
 
   Future<String> getSoundPath(String assetPath) async{
-    return await _assetPathToFilePath(assetPath);
+    final pref = (await prefs);
+    final value = pref.getString(assetPath);
+    if(value != null){
+      print('Loading from shared_pref');
+      return value;
+    }else{
+      print('Loading from assets');
+      final path = await _assetPathToFilePath(assetPath);
+      pref.setString(assetPath, path);
+      return path;
+    }
   }
 
   /// Writes new file to [tempPath] with byte data from asset and returns it.
   Future<String> _assetPathToFilePath(String assetPath) async{
     final name = assetPath.hashCode.toString();
     final tempPath = (await getTemporaryDirectory()).path;
-    // TODO delete mp3 if possible
     final file = new File('$tempPath/$name');
     final rawAsset = await rootBundle.load(assetPath);
     // TODO Handle io errors
